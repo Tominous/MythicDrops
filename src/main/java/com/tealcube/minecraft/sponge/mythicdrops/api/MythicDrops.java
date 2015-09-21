504aec3cfff149611ab03a6ec6e7b378030957e8
Richard Harrah <topplethenunnery@gmail.com>
2015-09-20 23:24:46 -0400

    cleaning MythicDrops interface

--- src/main/java/com/tealcube/minecraft/sponge/mythicdrops/api/MythicDrops.java
+++ src/main/java/com/tealcube/minecraft/sponge/mythicdrops/api/MythicDrops.java
@@ -23,47 +23,14 @@
  */
 package com.tealcube.minecraft.sponge.mythicdrops.api;
 
-import com.tealcube.minecraft.sponge.mythicdrops.api.registries.Registry;
-
-import org.slf4j.Logger;
-
-import java.lang.reflect.Type;
-
-import ninja.leaping.configurate.ConfigurationNode;
-
 /**
- * Represents the API of the Hoard plugin for Sponge.
+ * Represents the API of the MythicDrops plugin for Sponge.
  *
- * Hoard is the Sponge port of the Bukkit/Spigot plugin MythicDrops.
+ * This is the Sponge port of the Bukkit/Spigot plugin MythicDrops.
  *
  * @author Richard Harrah
  * @version 09162015
  */
 public interface MythicDrops {
-
-    /**
-     * Fetches the {@code Logger} used by the plugin to help debug and show messages.
-     *
-     * @return Logger for messages
-     */
-    Logger getLogger();
-
-    /**
-     * Fetches the {@code ConfigurationNode} for the default configuration file.
-     * @return ConfigurationNode for mythicdrops.conf
-     */
-    ConfigurationNode getDefaultConfigurationNode();
-
-    /**
-     * Fetches the {@code ConfigurationNode} for the tier configuration file.
-     * @return ConfigurationNode for tier.conf
-     */
-    ConfigurationNode getTierConfigurationNode();
-
-    /**
-     * Fetches the Registry of Registries.
-     * @return Registry for use
-     */
-    Registry<Type, Registry<?, ?>> getRegistry();
 
 }
