7820b6a3b5e844547479c38c730c53f2e10f7a49
Richard Harrah <topplethenunnery@gmail.com>
2015-09-21 16:44:10 -0400

    migrating to separate projects for API vs plugin

---
+++ mythicdrops-api/src/templates/java/com/tealcube/minecraft/sponge/mythicdrops/api/ApiVersion.java
@@ -0,0 +1,13 @@
+package com.tealcube.minecraft.sponge.mythicdrops.api;
+
+public final class ApiVersion {
+
+    public static final String ARTIFACT = "@ARTIFACT@";
+    public static final String VERSION = "@VERSION@";
+    public static final String NAME = "@NAME@";
+
+    private ApiVersion() {
+        // we don't want a visible constructor
+    }
+
+}
---
+++ mythicdrops-plugin/src/templates/java/com/tealcube/minecraft/sponge/mythicdrops/plugin/PluginVersion.java
@@ -0,0 +1,13 @@
+package com.tealcube.minecraft.sponge.mythicdrops.plugin;
+
+public final class PluginVersion {
+
+    public static final String ARTIFACT = "@ARTIFACT@";
+    public static final String VERSION = "@VERSION@";
+    public static final String NAME = "@NAME@";
+
+    private PluginVersion() {
+        // we don't want a visible constructor
+    }
+
+}
