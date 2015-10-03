63817e81c01f14182ea68feb28972fa6a1546d41
Richard Harrah <topplethenunnery@gmail.com>
2015-10-03 14:56:09 -0400

    moving to prepare for multiplatform support

--- mythicdrops-annotations/src/templates/java/com/tealcube/minecraft/spigot/annotations/PluginVersion.java
+++ mythicdrops-annotations/src/templates/java/com/tealcube/minecraft/spigot/annotations/PluginVersion.java
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.tealcube.minecraft.sponge.mythicdrops.plugin;
+package com.tealcube.minecraft.spigot.annotations;
 
 public final class PluginVersion {
 
