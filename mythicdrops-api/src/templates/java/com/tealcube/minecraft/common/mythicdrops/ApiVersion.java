547855e99363d343c426fd6eafb6d19d931b47ad
Richard Harrah <topplethenunnery@gmail.com>
2015-10-06 13:00:46 -0400

    adding ApiVersion class

---
+++ mythicdrops-api/src/templates/java/com/tealcube/minecraft/common/mythicdrops/ApiVersion.java
@@ -0,0 +1,16 @@
+package com.tealcube.minecraft.common.mythicdrops;
+
+/**
+ * Represents data taken from the POM of mythicdrops-api.
+ */
+public final class ApiVersion {
+
+    public static final String NAME = "@NAME@";
+    public static final String ARTIFACT = "@ARTIFACT@";
+    public static final String VERSION = "@VERSION@";
+
+    private ApiVersion() {
+        // make it private so it can't be instantiated
+    }
+
+}
