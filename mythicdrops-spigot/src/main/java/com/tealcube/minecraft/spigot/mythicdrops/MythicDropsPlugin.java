8b7b5896559f1f93ef89389871e455d91cfd5d58
Richard Harrah <topplethenunnery@gmail.com>
2015-10-06 12:52:10 -0400

    preparing for MythicDrops in two server environments

---
+++ mythicdrops-spigot/src/main/java/com/tealcube/minecraft/spigot/mythicdrops/MythicDropsPlugin.java
@@ -0,0 +1,14 @@
+package com.tealcube.minecraft.spigot.mythicdrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.Author;
+import org.bukkit.plugin.java.annotation.Main;
+import org.bukkit.plugin.java.annotation.Name;
+import org.bukkit.plugin.java.annotation.Version;
+
+@Main
+@Name(PluginVersion.NAME)
+@Version(PluginVersion.VERSION)
+@Author("ToppleTheNun")
+public final class MythicDropsPlugin extends JavaPlugin {
+}
