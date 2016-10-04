21ade28cfc5686624989c6f866bd438535b6343a
Richard Harrah <topplethenunnery@gmail.com>
2016-10-04 00:49:52 -0400

    adding MythicEnchantment and MythicItemType classes

---
+++ mythicdrops-api/src/main/java/com/tealcubegames/minecraft/spigot/mythicdrops/api/items/MythicEnchantment.java
@@ -0,0 +1,54 @@
+/**
+ * This file is part of MythicDrops API, licensed under the MIT License.
+ *
+ * Copyright (C) 2013 Teal Cube Games
+ *
+ * Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.tealcubegames.minecraft.spigot.mythicdrops.api.items;
+
+import org.bukkit.enchantments.Enchantment;
+import org.immutables.value.Value;
+
+/**
+ * An interface that represents an enchantment with a minimum and maximum level.
+ */
+@Value.Style(visibility = Value.Style.ImplementationVisibility.PACKAGE)
+@Value.Immutable
+public abstract class MythicEnchantment {
+
+    public abstract Enchantment enchantment();
+
+    public abstract int minimumLevel();
+
+    public abstract int maximumLevel();
+
+    public static Builder builder() {
+        return ImmutableMythicEnchantment.builder();
+    }
+
+    public interface Builder {
+        Builder enchantment(Enchantment enchantment);
+
+        Builder minimumLevel(int minimumLevel);
+
+        Builder maximumLevel(int maximumLevel);
+
+        MythicEnchantment build();
+    }
+
+}
---
+++ mythicdrops-api/src/main/java/com/tealcubegames/minecraft/spigot/mythicdrops/api/items/MythicItemType.java
@@ -0,0 +1,54 @@
+/**
+ * This file is part of MythicDrops API, licensed under the MIT License.
+ *
+ * Copyright (C) 2013 Teal Cube Games
+ *
+ * Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.tealcubegames.minecraft.spigot.mythicdrops.api.items;
+
+import org.bukkit.Material;
+import org.immutables.value.Value;
+
+import java.util.Collection;
+
+/**
+ * An interface that represents an item type.
+ *
+ * @author Richard Harrah
+ */
+@Value.Style(visibility = Value.Style.ImplementationVisibility.PACKAGE)
+@Value.Immutable
+public abstract class MythicItemType {
+
+    public abstract String name();
+
+    public abstract Collection<Material> materials();
+
+    public static Builder builder() {
+        return ImmutableMythicItemType.builder();
+    }
+
+    public interface Builder {
+        Builder name(String name);
+
+        Builder materials(Collection<Material> materials);
+
+        MythicItemType build();
+    }
+
+}
