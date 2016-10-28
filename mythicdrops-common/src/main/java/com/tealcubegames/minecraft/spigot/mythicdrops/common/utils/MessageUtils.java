8e7e6ab62046e31e856e5560f0ac3c2055b48b6c
Richard Harrah <topplethenunnery@gmail.com>
2016-10-27 23:27:23 -0400

    adding CaselessMap and TextManipulator and updating MessageUtils

--- mythicdrops-common/src/main/java/com/tealcubegames/minecraft/spigot/mythicdrops/common/utils/MessageUtils.java
+++ mythicdrops-common/src/main/java/com/tealcubegames/minecraft/spigot/mythicdrops/common/utils/MessageUtils.java
@@ -1,17 +1,17 @@
 /**
  * This file is part of MythicDrops_Common, licensed under the MIT License.
- *
+ * <p>
  * Copyright (C) 2013 Teal Cube Games
- *
+ * <p>
  * Permission is hereby granted, free of charge,
  * to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use,
  * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
  * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
@@ -19,19 +19,28 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package com.tealcubegames.minecraft.spigot.mythicdrops.common;
+package com.tealcubegames.minecraft.spigot.mythicdrops.common.utils;
 
 import com.google.common.base.Preconditions;
 import com.tealcube.minecraft.bukkit.TextUtils;
 import org.bukkit.command.CommandSender;
+
+import javax.inject.Inject;
 
 /**
  * A utility class for sending non-fancy messages to players.
  */
 public final class MessageUtils {
 
-    private MessageUtils() {
-        // do nothing
+    private final TextManipulator textManipulator;
+
+    @Inject
+    public MessageUtils(TextManipulator textManipulator) {
+        this.textManipulator = textManipulator;
+    }
+
+    public TextManipulator getTextManipulator() {
+        return textManipulator;
     }
 
     /**
@@ -40,10 +49,10 @@ public final class MessageUtils {
      * @param sender  Player or Console
      * @param message message to send
      */
-    public static void sendMessage(CommandSender sender, String message) {
+    public void sendMessage(CommandSender sender, String message) {
         Preconditions.checkNotNull(sender, "sender cannot be null");
         Preconditions.checkNotNull(message, "message cannot be null");
-        sender.sendMessage(TextUtils.color(message));
+        sender.sendMessage(textManipulator.color(message));
     }
 
     /**
@@ -56,11 +65,11 @@ public final class MessageUtils {
      * @param message message to send
      * @param args    tokens to replace and their values
      */
-    public static void sendMessage(CommandSender sender, String message, String[][] args) {
+    public void sendMessage(CommandSender sender, String message, String[][] args) {
         Preconditions.checkNotNull(sender, "sender cannot be null");
         Preconditions.checkNotNull(message, "message cannot be null");
         Preconditions.checkNotNull(args, "args cannot be null");
-        sender.sendMessage(TextUtils.color(TextUtils.args(message, args)));
+        sender.sendMessage(textManipulator.color(TextUtils.args(message, args)));
     }
 
 }
