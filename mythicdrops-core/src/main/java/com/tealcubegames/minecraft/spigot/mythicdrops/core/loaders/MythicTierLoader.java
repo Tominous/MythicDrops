36da0035b0c8905a4af7227708bb5b20dc477b76
Richard Harrah <topplethenunnery@gmail.com>
2016-11-08 21:43:02 -0500

    moving to gradle from maven

--- mythicdrops-core/src/main/java/com/tealcubegames/minecraft/spigot/mythicdrops/core/loaders/MythicTierLoader.java
+++ mythicdrops-core/src/main/java/com/tealcubegames/minecraft/spigot/mythicdrops/core/loaders/MythicTierLoader.java
@@ -1,17 +1,17 @@
 /**
  * This file is part of MythicDrops_Core, licensed under the MIT License.
- *
+ * <p>
  * Copyright (C) 2013 Teal Cube Games
- *
+ * <p>
  * Permission is hereby granted, free of charge,
  * to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use,
  * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
  * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
@@ -25,13 +25,11 @@ import com.google.inject.assistedinject.Assisted;
 import com.tealcubegames.minecraft.spigot.mythicdrops.api.loaders.LoaderManager;
 import com.tealcubegames.minecraft.spigot.mythicdrops.api.tiers.MythicTier;
 import com.tealcubegames.minecraft.spigot.mythicdrops.common.loaders.AbstractConfigurateMythicLoader;
-import ninja.leaping.configurate.ConfigurationNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.io.File;
-import java.io.IOException;
 
 /**
  * Implementation of MythicLoader for loading MythicTiers.
