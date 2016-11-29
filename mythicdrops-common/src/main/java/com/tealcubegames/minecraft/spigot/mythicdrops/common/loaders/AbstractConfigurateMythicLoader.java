e844171222ccec00773c9c7f8e9dabda6473d258
Richard Harrah <topplethenunnery@gmail.com>
2016-11-28 20:47:34 -0500

    updating license headers

--- mythicdrops-common/src/main/java/com/tealcubegames/minecraft/spigot/mythicdrops/common/loaders/AbstractConfigurateMythicLoader.java
+++ mythicdrops-common/src/main/java/com/tealcubegames/minecraft/spigot/mythicdrops/common/loaders/AbstractConfigurateMythicLoader.java
@@ -1,7 +1,7 @@
 /**
  * This file is part of mythicdrops-common, licensed under the MIT License.
  *
- * Copyright (C) 2016 Richard Harrah <topplethenunnery@gmail.com>
+ * Copyright (C) 2016 Pixel Outlaw <topplethenun@pixeloutlaw.io>
  *
  * Permission is hereby granted, free of charge,
  * to any person obtaining a copy of this software and associated documentation files (the "Software"),
