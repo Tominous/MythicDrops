fb7a2187e0cb1d6bdfab03f4291d96f320738580
Richard Harrah <topplethenunnery@gmail.com>
2018-10-17 21:12:32 -0400

    setting up for rewrite

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
+plugins {
+    base
+    kotlin("jvm") version "1.2.61" apply false
+    id("nebula.project") version "5.1.2"
+    id("com.diffplug.gradle.spotless") version "3.15.0" apply false
+}
+
+allprojects {
+    group = "io.pixeloutlaw.minecraft.spigot"
+    version = "420.0.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+        jcenter()
+        maven {
+            url = uri("https://hub.spigotmc.org/nexus/content/groups/public")
+        }
+    }
+}
+
+subprojects {
+    afterEvaluate {
+        this.plugins.apply("com.diffplug.gradle.spotless")
+        configure<SpotlessExtension> {
+            kotlin {
+                ktlint()
+            }
+        }
+    }
+}
---
+++ hilt/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    implementation("org.spigotmc:spigot-api:1.13.1-R0.1-SNAPSHOT")
+}
