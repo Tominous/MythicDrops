f5c4f47416399206dc34fe3a399ddbcf3fac531b
Richard Harrah <topplethenunnery@gmail.com>
2018-10-24 19:50:38 -0400

    adding MapItemStackExtensions

---
+++ hilt/src/main/kotlin/io/pixeloutlaw/minecraft/spigot/hilt/PotionItemStackExtensions.kt
@@ -0,0 +1,10 @@
+package io.pixeloutlaw.minecraft.spigot.hilt
+
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.PotionMeta
+
+fun ItemStack.getOrCreatePotionMeta() =
+this.getOrCreateItemMeta() as? PotionMeta
+
+fun ItemStack.hasPotionMeta() =
+    this.getOrCreateItemMeta() is PotionMeta
