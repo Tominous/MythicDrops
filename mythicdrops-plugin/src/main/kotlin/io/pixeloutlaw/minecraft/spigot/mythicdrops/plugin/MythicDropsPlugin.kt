ff5b8a70b38d995106003f7bbeebc8b1c4df3ddf
Richard Harrah <topplethenunnery@gmail.com>
2018-10-25 22:17:38 -0400

    adding very basic event handler for testing

--- mythicdrops-plugin/src/main/kotlin/io/pixeloutlaw/minecraft/spigot/mythicdrops/plugin/MythicDropsPlugin.kt
+++ mythicdrops-plugin/src/main/kotlin/io/pixeloutlaw/minecraft/spigot/mythicdrops/plugin/MythicDropsPlugin.kt
@@ -1,16 +1,26 @@
 package io.pixeloutlaw.minecraft.spigot.mythicdrops.plugin
 
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.plugin.java.annotation.plugin.Plugin
 
 @Plugin(name = BuildConfig.NAME, version = BuildConfig.VERSION)
-class MythicDropsPlugin : JavaPlugin() {
+class MythicDropsPlugin : JavaPlugin(), Listener {
 
     override fun onEnable() {
         logger.info("onEnable")
+        server.pluginManager.registerEvents(this, this)
     }
 
     override fun onDisable() {
         logger.info("onDisable")
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    fun onPlayerJoinEvent(event: PlayerJoinEvent) {
+        logger.info("Hi mom!")
     }
 }
