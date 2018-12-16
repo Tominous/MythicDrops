c18b74073c14929c76fb4efafb9adeba1dc22f43
Richard Harrah <topplethenunnery@gmail.com>
2018-12-16 15:44:06 -0500

    using hilt in blockdamageevent

--- plugin/src/main/kotlin/io/pixeloutlaw/minecraft/spigot/mythicdrops/plugin/MythicDropsPlugin.kt
+++ plugin/src/main/kotlin/io/pixeloutlaw/minecraft/spigot/mythicdrops/plugin/MythicDropsPlugin.kt
@@ -1,10 +1,13 @@
 package io.pixeloutlaw.minecraft.spigot.mythicdrops.plugin
 
+import com.google.common.base.Strings
+import io.pixeloutlaw.minecraft.spigot.hilt.getDisplayName
+import io.pixeloutlaw.minecraft.spigot.hilt.setDisplayName
 import io.pixeloutlaw.minecraft.spigot.plugin.BuildConfig
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.block.BlockDamageEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.plugin.java.annotation.plugin.Plugin
 
@@ -21,7 +24,12 @@ class MythicDropsPlugin : JavaPlugin(), Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    fun onPlayerJoinEvent(event: PlayerJoinEvent) {
-        logger.info("Hi mom!")
+    fun onBlockDamageEvent(event: BlockDamageEvent) {
+        if (event.player == null) {
+            return
+        }
+        val itemInHand = event.itemInHand
+        itemInHand.setDisplayName("${Strings.nullToEmpty(itemInHand.getDisplayName())}d")
+        event.player.updateInventory()
     }
 }
