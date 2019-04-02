9291cc08e191c0220ffa5468ad0413a183fc7f14
Richard Harrah <topplethenunnery@gmail.com>
2019-04-01 21:03:33 -0400

    remove more configuration from hilt

--- hilt/build.gradle.kts
+++ hilt/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm")
 }
@@ -7,13 +5,4 @@ plugins {
 dependencies {
     implementation(Libs.spigot_api)
     implementation(Libs.kotlin_stdlib_jdk8)
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
